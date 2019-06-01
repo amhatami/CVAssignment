1f20a357833799258b45bfe1aed803db41f7fc17
Pooya Hatami <am.hatami@gmail.com>
2019-06-01 08:03:05 -0700

    Initial Commit

---
+++ CVAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CVAssignment
+//
+//  Created by Pooya on 2019-06-01.
+//  Copyright © 2019 AmirHatami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
